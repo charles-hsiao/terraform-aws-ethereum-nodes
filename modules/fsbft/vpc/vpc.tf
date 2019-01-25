6fcd9a1cba8293e9374414fd2b38d09dca7f3d8e
Charles Hsiao <charles_hsiao@maicoin.com>
2019-01-25 17:54:52 +0800

    Add public subnet

--- modules/fsbft/vpc/vpc.tf
+++ modules/fsbft/vpc/vpc.tf
@@ -1,8 +1,20 @@
+# VPC
 resource "aws_vpc" "fsbft-vpc" {
   cidr_block = "${var.vpc_cidr}"
   enable_dns_hostnames = true
 
   tags {
     Name = "${var.environment}-${var.project}-vpc"
+  }
+}
+
+# Public subnet
+resource "aws_subnet" "fsbft-public-subnet-a" {
+  vpc_id = "${aws_vpc.fsbft-vpc.id}"
+  cidr_block = "${var.public_subnet_cidr}"
+  availability_zone = "${var.aws_region}a"
+
+  tags {
+    Name = "${var.environment}-${var.project}-public-a"
   }
 }
