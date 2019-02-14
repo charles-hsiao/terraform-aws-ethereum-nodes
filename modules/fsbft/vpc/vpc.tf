ab78242b267cdb7d194dcf952df6e79bc0e1f788
Charles Hsiao <charles_hsiao@maicoin.com>
2019-02-14 16:29:10 +0800

    Add IGW & RT for public VPC

--- modules/fsbft/vpc/vpc.tf
+++ modules/fsbft/vpc/vpc.tf
@@ -10,6 +10,39 @@ resource "aws_vpc" "fsbft-vpc" {
   }
 }
 
+# Define the internet gateway
+resource "aws_internet_gateway" "fsbft-gw" {
+  vpc_id = "${aws_vpc.fsbft-vpc.id}"
+
+  tags {
+    Name = "${var.environment}-${var.project}-gw"
+    Project = "${var.project}"
+    Environment = "${var.environment}"
+  }
+}
+
+# Define the route table
+resource "aws_route_table" "fsbft-rt" {
+  vpc_id = "${aws_vpc.fsbft-vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.fsbft-gw.id}"
+  }
+
+  tags {
+    Name = "${var.environment}-${var.project}-rt"
+    Project = "${var.project}"
+    Environment = "${var.environment}"
+  }
+}
+
+# Assign the route table to the public Subnet
+resource "aws_route_table_association" "fsbft-rt-s" {
+  subnet_id = "${aws_subnet.fsbft-public-subnet-a.id}"
+  route_table_id = "${aws_route_table.fsbft-rt.id}"
+}
+
 # Public subnet
 resource "aws_subnet" "fsbft-public-subnet-a" {
   vpc_id = "${aws_vpc.fsbft-vpc.id}"
@@ -23,3 +56,5 @@ resource "aws_subnet" "fsbft-public-subnet-a" {
     SubnetType = "public"
   }
 }
+
+
