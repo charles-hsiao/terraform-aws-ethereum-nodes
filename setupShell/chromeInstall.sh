36671e1c0d292e8a0e0705e04a106aa3e94b2c8e
Charles Hsiao <charles_hsiao@maicoin.com>
2019-02-20 23:34:17 +0800

    Clean-up

--- setupShell/chromeInstall.sh
+++
@@ -1,6 +0,0 @@
-sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
-wget https://dl-ssl.google.com/linux/linux_signing_key.pub
-sudo apt-key add linux_signing_key.pub
-
-sudo apt-get update
-sudo apt-get install google-chrome-stable
